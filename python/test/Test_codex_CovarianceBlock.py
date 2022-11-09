5651d99327f35439ac915f8fc33bb10ae8cb0a1f
Wim Haeck <whaeck@gmail.com>
2022-11-09 09:18:02 -0700

    More tests

--- python/test/Test_codex_CovarianceBlock.py
+++ python/test/Test_codex_CovarianceBlock.py
@@ -12,7 +12,7 @@ class Test_codex_CovarianceBlock( unittest.TestCase ) :
 
     def test_component( self ) :
 
-        # the data is given explicitly
+        # the data is given explicitly - for a diagonal block
         chunk = CovarianceBlock( nuclide = 'U235', reaction = 'elastic',
                                  energies = [ 1e-5, 1., 1e+6, 2e+7 ],
                                  matrix = numpy.array( [ [ 1., 2., 3. ],
@@ -48,6 +48,41 @@ class Test_codex_CovarianceBlock( unittest.TestCase ) :
         self.assertAlmostEqual( 3., chunk.matrix[2,0] )
         self.assertAlmostEqual( 5., chunk.matrix[2,1] )
         self.assertAlmostEqual( 6., chunk.matrix[2,2] )
+
+        # the data is given explicitly - for an off-diagonal block
+        chunk = CovarianceBlock( row_nuclide = 'U235', row_reaction = 'elastic',
+                                 row_energies = [ 1e-5, 1., 1e+6, 2e+7 ],
+                                 column_nuclide = 'U238', column_reaction = 'fission',
+                                 column_energies = [ 1e-5, 2., 2e+7 ],
+                                 matrix = numpy.array( [ [ 1., 2. ],
+                                                         [ 3., 4. ],
+                                                         [ 5., 6. ] ] ) )
+
+        # verify content
+        self.assertEqual( 'U235', chunk.row.nuclide )
+        self.assertEqual( 'elastic', chunk.row.reaction )
+        self.assertEqual( 4, len( chunk.row.energies ) )
+        self.assertAlmostEqual( 1e-5, chunk.row.energies[0] )
+        self.assertAlmostEqual( 1.  , chunk.row.energies[1] )
+        self.assertAlmostEqual( 1e+6, chunk.row.energies[2] )
+        self.assertAlmostEqual( 2e+7, chunk.row.energies[3] )
+
+        self.assertEqual( 'U238', chunk.column.nuclide )
+        self.assertEqual( 'fission', chunk.column.reaction )
+        self.assertEqual( 3, len( chunk.column.energies ) )
+        self.assertAlmostEqual( 1e-5, chunk.column.energies[0] )
+        self.assertAlmostEqual( 2.  , chunk.column.energies[1] )
+        self.assertAlmostEqual( 2e+7, chunk.column.energies[2] )
+
+        self.assertEqual( True, chunk.is_off_diagonal )
+        self.assertEqual( False, chunk.is_diagonal )
+
+        self.assertAlmostEqual( 1., chunk.matrix[0,0] )
+        self.assertAlmostEqual( 2., chunk.matrix[0,1] )
+        self.assertAlmostEqual( 3., chunk.matrix[1,0] )
+        self.assertAlmostEqual( 4., chunk.matrix[1,1] )
+        self.assertAlmostEqual( 5., chunk.matrix[2,0] )
+        self.assertAlmostEqual( 6., chunk.matrix[2,1] )
 
     def test_failures( self ) :
 
